aac8011c03cb7c4ee431c8f3d4faa3f0f258946b
Luis Paz Cordova <lpaz0073@gmail.com>
2024-05-09 18:05:05 -0500

    feat: Add resume cards to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,52 @@
+import { ResumeCard } from "@/components";
+
+// [
+// 	{ workout: "Workout A", date: "2021-09-01" },
+// 	{ workout: "Workout B", date: "2021-09-02" },
+// 	{ workout: "Workout C", date: "2021-09-03" },
+// ];
+
 export default function DashboardPage() {
 	return (
-		<div className=''>
-			<h1>Hello Dashboard</h1>
+		<div className='grid grid-cols-1 gap-6'>
+			<ResumeCard>
+				<ResumeCard.Header
+					title='Recent Workouts'
+					subTitle='Your recent workouts'
+				/>
+				<ResumeCard.Body>
+					<div className='flex flex-col gap-2'>
+						<p>Press Day</p>
+						<p>Pull Day</p>
+						<p>Leg Day</p>
+					</div>
+					<div className='flex flex-col gap-2'>
+						<p>06/05/24</p>
+						<p>06/05/24</p>
+						<p>06/05/24</p>
+					</div>
+				</ResumeCard.Body>
+				<ResumeCard.Link text='View all workouts' href='/workouts' />
+			</ResumeCard>
+			<ResumeCard>
+				<ResumeCard.Header
+					title='Completed Exercises'
+					subTitle='Exercises you have completed recently'
+				/>
+				<ResumeCard.Body>
+					<div className='flex flex-col gap-2'>
+						<p>Bicep Curls</p>
+						<p>Shoulder Press</p>
+						<p>Lat Pulldowns</p>
+					</div>
+					<div className='flex flex-col gap-2'>
+						<p>12 reps</p>
+						<p>10 reps</p>
+						<p>15 reps</p>
+					</div>
+				</ResumeCard.Body>
+				<ResumeCard.Link text='View all exercises' href='/exercises' />
+			</ResumeCard>
 		</div>
 	);
 }
