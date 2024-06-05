ab781159be8a3bceeac487e579348e18801814e1
Luis Paz Cordova <lpaz0073@gmail.com>
2024-06-05 09:48:08 -0500

    feat: add muscle group filter to ExerciseSection form

--- src/app/exercises/components/ExerciseSection.tsx
+++ src/app/exercises/components/ExerciseSection.tsx
@@ -1,13 +1,85 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ResumeCard } from "@/components";
+import { Button } from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+} from "@/components/ui/form";
+
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
 import { exercisesResume } from "@/data";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const groups = [
+	{ label: "Chest", value: "chest" },
+	{ label: "Back", value: "back" },
+	{ label: "Legs", value: "legs" },
+	{ label: "Shoulders", value: "shoulders" },
+	{ label: "Biceps", value: "biceps" },
+	{ label: "Triceps", value: "triceps" },
+	{ label: "Abs", value: "abs" },
+];
+
+const FormSchema = z.object({
+	muscleGroup: z.string({
+		required_error: "Por favor selecciona un grupo muscular",
+	}),
+});
 
 export const ExerciseSection = () => {
+	const form = useForm<z.infer<typeof FormSchema>>({
+		resolver: zodResolver(FormSchema),
+	});
+
+	const onSubmit = (data: z.infer<typeof FormSchema>) => {
+		const { muscleGroup } = data;
+		console.log(muscleGroup);
+	};
 	return (
-		<section className='mt-4'>
-			{/* TODO: Add filter to filtering exercises in according their muscle group */}
-			{/* Use "Combobox" componnet by Shadcn */}
+		<section className='mt-4 flex flex-col gap-6'>
+			<Form {...form}>
+				<form className='space-y-4' onSubmit={form.handleSubmit(onSubmit)}>
+					<FormField
+						control={form.control}
+						name='muscleGroup'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Grupo muscular</FormLabel>
+								<Select
+									onValueChange={field.onChange}
+									defaultValue={field.value}
+								>
+									<FormControl>
+										<SelectTrigger>
+											<SelectValue placeholder='Selecciona un grupo muscular' />
+										</SelectTrigger>
+									</FormControl>
+									<SelectContent>
+										{groups.map((group, index) => (
+											<SelectItem key={index} value={group.value}>
+												{group.label}
+											</SelectItem>
+										))}
+									</SelectContent>
+								</Select>
+							</FormItem>
+						)}
+					/>
+					<Button type='submit'>Filtrar</Button>
+				</form>
+			</Form>
 			<div className='flex flex-col gap-4'>
 				{exercisesResume.map((exercise, index) => (
 					<ResumeCard key={index}>
