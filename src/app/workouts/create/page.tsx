eb68e0a4fe7d704903ca3d5006e7ba32118ac979
Luis Paz Cordova <lpaz0073@gmail.com>
2024-06-05 09:49:13 -0500

    feat: add content to CreateWorkoutPage

--- src/app/workouts/create/page.tsx
+++ src/app/workouts/create/page.tsx
@@ -1,7 +1,17 @@
+import { WorkoutForm } from "@/components";
+
 export default function CreateWorkoutPage() {
 	return (
-		<div>
-			<h1>Create Workout</h1>
-		</div>
+		<section className='flex flex-col gap-6'>
+			<div className='flex flex-col gap-4 border border-gray-300 shadow-md p-6 rounded-md'>
+				<div>
+					<h2>Crear entrenamiento</h2>
+					<p className='text-xs text-gray-400 mt-2'>
+						Agrega tus ejercicios con sus respectivas series y repeticiones
+					</p>
+				</div>
+				<WorkoutForm />
+			</div>
+		</section>
 	);
 }
