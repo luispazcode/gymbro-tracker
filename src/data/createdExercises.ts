207eafa534c10a5e117dce063879c4de8922e0a6
Luis Paz Cordova <lpaz0073@gmail.com>
2024-06-25 14:39:11 -0500

    feat: add mock data for created exercises

---
+++ src/data/createdExercises.ts
@@ -0,0 +1,42 @@
+export interface CreatedExercise {
+	name: string;
+	value: string;
+}
+
+export const createdExercises: CreatedExercise[] = [
+	{ name: "Peso muerto con barra", value: "bar-deadlift" },
+	{ name: "Sentadillas con barra", value: "bar-squats" },
+	{
+		name: "Elevación lateral de hombros con mancuernas",
+		value: "dumbbell-lateral-raises",
+	},
+	{ name: "Flexiones de brazos", value: "push-ups" },
+	{ name: "Pull-ups", value: "pull-ups" },
+	{ name: "Press militar con barra", value: "bar-military-press" },
+	{ name: "Fondos de tríceps en banco", value: "bench-tricep-dips" },
+	{ name: "Remo con barra", value: "barbell-rows" },
+	{
+		name: "Prensa de hombros con mancuernas",
+		value: "dumbbell-shoulder-press",
+	},
+	{ name: "Zancadas con mancuernas", value: "dumbbell-lunges" },
+	{ name: "Curl de bíceps de pie con barra", value: "standing-barbell-curl" },
+	{
+		name: "Press de banca inclinado con barra",
+		value: "bar-incline-bench-press",
+	},
+	{
+		name: "Extensiones de tríceps con mancuerna",
+		value: "dumbbell-tricep-extensions",
+	},
+	{
+		name: "Peso muerto rumano con mancuernas",
+		value: "dumbbell-romanian-deadlift",
+	},
+	{ name: "Sentadillas con salto", value: "jump-squats" },
+	{ name: "Pájaros con mancuernas", value: "dumbbell-reverse-flyes" },
+	{ name: "Plancha", value: "plank" },
+	{ name: "Levantamiento de talones con barra", value: "barbell-calf-raises" },
+	{ name: "Flexiones diamante", value: "diamond-push-ups" },
+	{ name: "Dominadas agarre ancho", value: "wide-grip-pull-ups" },
+];
