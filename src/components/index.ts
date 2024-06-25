25de820e3e804a5e28a66a4ffd0ea3da4938fc80
Luis Paz Cordova <lpaz0073@gmail.com>
2024-06-25 14:32:18 -0500

    feat: add workout components to the barrel file

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,9 @@ export { Sidebar } from "./Sidebar";
 export { Header } from "./Header";
 export { ResumeCard } from "./ResumeCard";
 export { ExerciseForm } from "./ExerciseForm";
-export { WorkoutForm } from "./workout/WorkoutForm";
+
+// Workouts
+export { AddExerciseForm } from "./workout/AddExerciseForm";
+export { DialogAddExercise } from "./workout/DialogAddExercise";
+export { SummaryWorkout } from "./workout/SummaryWorkout";
+export { SummaryWorkoutForm } from "./workout/SummaryWorkoutForm";
