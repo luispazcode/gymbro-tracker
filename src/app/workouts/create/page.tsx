e06d21a4fa52ffc3e694b913d2c1456670571f14
Luis Paz Cordova <lpaz0073@gmail.com>
2024-06-25 14:29:38 -0500

    feat: isolate forms and its logic in components

--- src/app/workouts/create/page.tsx
+++ src/app/workouts/create/page.tsx
@@ -1,17 +1,23 @@
-import { WorkoutForm } from "@/components";
+import { DialogAddExercise, SummaryWorkout } from "@/components";
 
 export default function CreateWorkoutPage() {
 	return (
-		<section className='flex flex-col gap-6'>
-			<div className='flex flex-col gap-4 border border-gray-300 shadow-md p-6 rounded-md'>
-				<div>
-					<h2>Crear entrenamiento</h2>
-					<p className='text-xs text-gray-400 mt-2'>
-						Agrega tus ejercicios con sus respectivas series y repeticiones
-					</p>
+		<main className='flex flex-col gap-6'>
+			<section className='flex flex-col gap-6'>
+				<div className='flex flex-col gap-4 border border-gray-300 shadow-md p-6 rounded-md'>
+					<div>
+						<h2>Crear entrenamiento</h2>
+						<p className='text-xs text-gray-400 mt-2'>
+							Vamos a crear tu entrenamiento, primero agrega cada uno de tus
+							ejercicios con sus series, repeticiones y pesos.
+						</p>
+					</div>
+					<div className='flex flex-col justify-center items-center'>
+						<DialogAddExercise />
+					</div>
 				</div>
-				<WorkoutForm />
-			</div>
-		</section>
+			</section>
+			<SummaryWorkout />
+		</main>
 	);
 }
