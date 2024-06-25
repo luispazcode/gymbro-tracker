cecf4fcc726a6a9b7fc4acba60b4aa7ebaa2d9cf
Luis Paz Cordova <lpaz0073@gmail.com>
2024-06-25 14:31:24 -0500

    refactor: replace useState for zustand in sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,29 +2,35 @@
 import { LiaDumbbellSolid } from "react-icons/lia";
 import { TiThMenu } from "react-icons/ti";
 import { Sidebar } from "./Sidebar";
-import { useState } from "react";
+import { useUIStore } from "@/store";
+import clsx from "clsx";
 
 export const Header = () => {
-	const [open, setOpen] = useState<boolean>(false);
+	const isSidebarOpen = useUIStore((state) => state.isSidebarOpen);
+	const openSidebar = useUIStore((state) => state.openSidebar);
+	const closeSidebar = useUIStore((state) => state.closeSidebar);
 	return (
 		<>
 			<header
-				className={`px-6 py-4 flex items-center justify-between ${
-					open ? "border-none" : "border-b"
-				}  border-gray-400 transition-all ease-in`}
+				className={clsx(
+					"px-6 py-4 flex items-center justify-between border-gray-400 transition-all ease-in",
+					isSidebarOpen ? "border-none" : "border-b"
+				)}
 			>
 				<div className='flex items-center gap-2'>
 					<LiaDumbbellSolid className='w-6 h-6' />
 					<h1 className='text-2xl font-bold'>Gymbro Tracker</h1>
 				</div>
 				<button
-					onClick={() => setOpen(!open)}
+					onClick={() => {
+						isSidebarOpen ? closeSidebar() : openSidebar();
+					}}
 					className='p-2 rounded-md border border-gray-400 hover:bg-slate-300 hover:text-black'
 				>
 					<TiThMenu />
 				</button>
 			</header>
-			<Sidebar isOpen={open} setOpen={setOpen} />
+			<Sidebar />
 		</>
 	);
 };
