e74f9801bbb713c4e02033988a84d0c8df0a2822
Luis Paz Cordova <lpaz0073@gmail.com>
2024-06-25 14:34:50 -0500

    feat: create DialogAddExercise component

---
+++ src/components/workout/DialogAddExercise.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useUIStore } from "@/store";
+import { Button } from "../ui/button";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "../ui/dialog";
+import { AddExerciseForm } from "./AddExerciseForm";
+
+export const DialogAddExercise = () => {
+	const isDialogOpen = useUIStore((state) => state.isDialogOpen);
+	const openDialog = useUIStore((state) => state.openDialog);
+	const closeDialog = useUIStore((state) => state.closeDialog);
+	const toggleDialog = isDialogOpen ? closeDialog : openDialog;
+
+	return (
+		<Dialog open={isDialogOpen} onOpenChange={toggleDialog}>
+			<DialogTrigger asChild>
+				<Button variant='outline'>Agregar ejercicio</Button>
+			</DialogTrigger>
+			<DialogContent className='sm:max-w-md'>
+				<DialogHeader>
+					<DialogTitle>Agrega tus ejercicios</DialogTitle>
+					<DialogDescription>
+						Selecciona tu ejercicio con sus series, repeticiones y pesos.
+					</DialogDescription>
+				</DialogHeader>
+				<div className='w-full mt-4'>
+					<AddExerciseForm />
+				</div>
+			</DialogContent>
+		</Dialog>
+	);
+};
