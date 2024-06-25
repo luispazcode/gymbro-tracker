8fe2ac2cbd9a8f6e0a9b50cb4488305829ec81ea
Luis Paz Cordova <lpaz0073@gmail.com>
2024-06-25 14:27:21 -0500

    style: update heading

--- src/app/exercises/page.tsx
+++ src/app/exercises/page.tsx
@@ -3,7 +3,7 @@ import { ExerciseSection } from "./components/ExerciseSection";
 export default function ExercisesPage() {
 	return (
 		<>
-			<h2>Mis ejercicios</h2>
+			<h1>Mis ejercicios</h1>
 			<p className='text-xs text-gray-400 mt-2'>
 				Aquí se mostrarán todos tus ejercicios registrados
 			</p>
