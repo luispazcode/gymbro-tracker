33a8c5b326825bd1e3f9c43965053d2345f61d4c
Luis Paz Cordova <lpaz0073@gmail.com>
2024-06-27 23:18:57 -0500

    feat: add background blur when sidebar is open

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,24 +19,24 @@ const menuItems: MenuItem[] = [
 		link: "/dashboard",
 	},
 	{
-		title: "Create Workout",
-		icon: <FiPlus className='w-4 h-4' />,
-		link: "/workouts/create",
-	},
-	{
-		title: "Workouts",
-		icon: <FiActivity className='w-4 h-4' />,
-		link: "/workouts",
-	},
-	{
-		title: "Create Exercise",
+		title: "Crear nuevo ejercicio",
 		icon: <FiPlus className='w-4 h-4' />,
 		link: "/exercises/create",
 	},
 	{
-		title: "Exercises",
+		title: "Mis ejercicios",
 		icon: <GiGymBag className='w-4 h-4' />,
 		link: "/exercises",
+	},
+	{
+		title: "Crear nuevo entrenamiento",
+		icon: <FiPlus className='w-4 h-4' />,
+		link: "/workouts/create",
+	},
+	{
+		title: "Mis entrenamientos",
+		icon: <FiActivity className='w-4 h-4' />,
+		link: "/workouts",
 	},
 ];
 
@@ -44,29 +44,43 @@ export const Sidebar = () => {
 	const isSidebarOpen = useUIStore((state) => state.isSidebarOpen);
 	const closeSidebar = useUIStore((state) => state.closeSidebar);
 	return (
-		<div
-			className={clsx(
-				"transition-all ease-linear",
-				isSidebarOpen
-					? "absolute w-full left-0 bg-white dark:bg-gray-500 border-b border-gray-400"
-					: "hidden"
-			)}
-		>
-			<div className='w-full px-6 pb-4 flex flex-col gap-10'>
-				<nav className='flex flex-col  gap-4'>
-					{menuItems.map((item: MenuItem) => (
-						<Link
-							href={item.link}
-							className='flex items-center gap-2 rounded-md px-3 py-2 hover:bg-gray-100 dark:hover:bg-gray-800'
-							key={item.title}
-							onClick={() => closeSidebar()}
-						>
-							{item.icon}
-							<p>{item.title}</p>
-						</Link>
-					))}
-				</nav>
+		<>
+			<div
+				className={clsx(
+					"",
+					isSidebarOpen
+						? "absolute z-20 w-full left-0 bg-white dark:bg-gray-500 border-b border-gray-400"
+						: "hidden"
+				)}
+			>
+				<div className='w-full px-6 pb-4 flex flex-col gap-10'>
+					<nav className='flex flex-col  gap-4'>
+						{menuItems.map((item: MenuItem) => (
+							<Link
+								href={item.link}
+								className='flex items-center gap-2 rounded-md px-3 py-2 hover:bg-gray-100 dark:hover:bg-gray-800'
+								key={item.title}
+								onClick={() => closeSidebar()}
+							>
+								{item.icon}
+								<p>{item.title}</p>
+							</Link>
+						))}
+					</nav>
+				</div>
 			</div>
-		</div>
+			{isSidebarOpen && (
+				<div
+					className='bg-black opacity-30 w-full h-screen fixed z-10'
+					onClick={() => closeSidebar()}
+				></div>
+			)}
+			{isSidebarOpen && (
+				<div
+					className='fade-in fixed bottom-0 left-0 w-full h-full z-10 backdrop-filter backdrop-blur-sm'
+					onClick={() => closeSidebar()}
+				></div>
+			)}
+		</>
 	);
 };
