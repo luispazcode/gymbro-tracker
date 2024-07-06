2425f6457e56adb69799ed64029b1eb59fb85d7c
Luis Paz Cordova <lpaz0073@gmail.com>
2024-07-05 23:24:05 -0500

    refactor: isolate ExerciseSection, fetch exercises and muscle groups in ExercisesPage

--- src/app/exercises/components/ExerciseSection.tsx
+++ src/app/exercises/components/ExerciseSection.tsx
@@ -1,111 +1,51 @@
 "use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
 import { ResumeCard } from "@/components";
-import { Button } from "@/components/ui/button";
-import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-} from "@/components/ui/form";
+import { FilterExercises } from "./FilterExercises";
+import Link from "next/link";
+import { Exercise, MuscleGroup } from "@prisma/client";
+import { useExercisesStore } from "@/store";
+import { useEffect } from "react";
 
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "@/components/ui/select";
-import { exercisesResume } from "@/data/resumeExercises";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+interface Props {
+	exerciseList: Exercise[];
+	muscleList: MuscleGroup[];
+}
 
-const groups = [
-	{ label: "Chest", value: "chest" },
-	{ label: "Back", value: "back" },
-	{ label: "Legs", value: "legs" },
-	{ label: "Shoulders", value: "shoulders" },
-	{ label: "Biceps", value: "biceps" },
-	{ label: "Triceps", value: "triceps" },
-	{ label: "Abs", value: "abs" },
-];
-
-const FormSchema = z.object({
-	muscleGroup: z.string({
-		required_error: "Por favor selecciona un grupo muscular",
-	}),
-});
-
-export const ExerciseSection = () => {
-	const form = useForm<z.infer<typeof FormSchema>>({
-		resolver: zodResolver(FormSchema),
-	});
-
-	const onSubmit = (data: z.infer<typeof FormSchema>) => {
-		const { muscleGroup } = data;
-		console.log(muscleGroup);
-	};
+export const ExerciseSection = ({ exerciseList, muscleList }: Props) => {
+	const { setExercises, filteredExercises } = useExercisesStore();
+	useEffect(() => {
+		setExercises(exerciseList);
+	}, [exerciseList, setExercises]);
 	return (
 		<section className='mt-4 flex flex-col gap-6'>
-			<Form {...form}>
-				<form className='space-y-4' onSubmit={form.handleSubmit(onSubmit)}>
-					<FormField
-						control={form.control}
-						name='muscleGroup'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Grupo muscular</FormLabel>
-								<Select
-									onValueChange={field.onChange}
-									defaultValue={field.value}
-								>
-									<FormControl>
-										<SelectTrigger>
-											<SelectValue placeholder='Selecciona un grupo muscular' />
-										</SelectTrigger>
-									</FormControl>
-									<SelectContent>
-										{groups.map((group, index) => (
-											<SelectItem key={index} value={group.value}>
-												{group.label}
-											</SelectItem>
-										))}
-									</SelectContent>
-								</Select>
-							</FormItem>
-						)}
-					/>
-					<Button type='submit'>Filtrar</Button>
-				</form>
-			</Form>
+			<FilterExercises mouscleGroups={muscleList} />
 			<div className='flex flex-col gap-4'>
-				{exercisesResume.map((exercise, index) => (
-					<ResumeCard key={index}>
-						<ResumeCard.Header
-							title={exercise.title}
-							subTitle={exercise.subTitle}
-						/>
-						<ResumeCard.Body>
-							<div className='flex flex-col gap-2 text-xs'>
-								<p className='text-sm'>Último entrenamiento:</p>
-								{exercise.series.map((serie, index) => (
-									<div
-										className='flex items-center justify-between'
-										key={index}
-									>
-										<p>Serie {index + 1}:</p>
-										<p className='flex gap-2'>
-											<span>{serie.reps} reps</span>
-											<span>{serie.weight} Kg</span>
-										</p>
-									</div>
-								))}
-							</div>
-						</ResumeCard.Body>
-					</ResumeCard>
-				))}
+				{filteredExercises.length > 0 ? (
+					filteredExercises.map((exercise, index) => (
+						<ResumeCard key={index}>
+							<ResumeCard.Header
+								title={exercise.name}
+								subTitle={
+									exercise.description ??
+									"No hay descripción para este ejercicio"
+								}
+							/>
+						</ResumeCard>
+					))
+				) : (
+					<>
+						<p>No has creado ningún ejercicio para este grupo muscular...</p>
+						<p>
+							¡Anímate{" "}
+							<Link
+								href='/exercises/create'
+								className='underline font-semibold'
+							>
+								a crear uno!
+							</Link>
+						</p>
+					</>
+				)}
 			</div>
 		</section>
 	);
--- src/app/exercises/page.tsx
+++ src/app/exercises/page.tsx
@@ -1,13 +1,16 @@
+import { getExercises, getMuscleGroups } from "@/actions";
 import { ExerciseSection } from "./components/ExerciseSection";
 
-export default function ExercisesPage() {
+export default async function ExercisesPage() {
+	const createdExercises = await getExercises();
+	const muscles = await getMuscleGroups();
 	return (
 		<>
 			<h1>Mis ejercicios</h1>
 			<p className='text-xs text-gray-400 mt-2'>
 				Aquí se mostrarán todos tus ejercicios registrados
 			</p>
-			<ExerciseSection />
+			<ExerciseSection exerciseList={createdExercises} muscleList={muscles} />
 		</>
 	);
 }
