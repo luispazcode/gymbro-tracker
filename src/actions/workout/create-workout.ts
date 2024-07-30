d272f11f262adfa3697348ec9ac247ae371ad605
Luis Paz Cordova <lpaz0073@gmail.com>
2024-07-30 01:44:58 -0500

    feat: create server action to create a workout

---
+++ src/actions/workout/create-workout.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { CreateWorkoutFormData } from "@/components/workout/SummaryWorkoutForm";
+import prisma from "@/lib/prisma";
+
+export const createWorkout = async (formData: CreateWorkoutFormData) => {
+	try {
+		const { listExercises } = formData;
+		const setsForRecording = listExercises.map((exercise) => {
+			return exercise.sets.map((set) => ({
+				reps: set.reps,
+				weight: set.weight,
+				exerciseId: exercise.exerciseValue,
+			}));
+		});
+		const newWorkout = await prisma.workout.create({
+			data: {
+				name: formData.nameWorkout,
+				date: formData.dateWorkout,
+				tag: formData.nameWorkout.toLowerCase().replace(/\s/g, "-"),
+				sets: {
+					create: setsForRecording.flat(),
+				},
+			},
+		});
+	} catch (error) {
+		console.error(error);
+	}
+};
