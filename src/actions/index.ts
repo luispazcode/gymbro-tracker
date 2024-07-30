35345e8d6965addbc3c568d00b6a9b2f84aef7b4
Luis Paz Cordova <lpaz0073@gmail.com>
2024-07-30 01:58:13 -0500

    feat: include createWorkout and getWorkouts into the barrel file actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,8 @@
 export { createExercise } from "./exercise/create-exercise";
-export { getExercises } from "./exercise/get-exercise";
+export { getExercises } from "./exercise/get-exercises";
 
 export { getMuscleGroups } from "./muscle/get-muscle-groups";
+
+export { createWorkout } from "./workout/create-workout";
+export { getWorkouts } from "./workout/get-workouts";
+export { getWorkoutBySlug } from "./workout/get-workout-by-slug";
