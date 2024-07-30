a3645cc83e388e29b8b368f75ef44266d248b9b1
Luis Paz Cordova <lpaz0073@gmail.com>
2024-07-30 01:49:18 -0500

    feat: display workouts info from database

--- src/app/workouts/page.tsx
+++ src/app/workouts/page.tsx
@@ -1,57 +1,39 @@
-import { ResumeCard } from "@/components";
-import { Button } from "@/components/ui/button";
-import { DumbbellIcon } from "lucide-react";
-import Link from "next/link";
+import { getWorkouts } from "@/actions";
+import { WorkoutsSection } from "./components/WorkoutsSection";
+import { WorkoutToDisplay } from "@/interfaces";
 
-export default function WorkoutsPage() {
+export default async function WorkoutsPage() {
+	const createdWorkouts: WorkoutToDisplay[] = await getWorkouts();
+
 	return (
 		<section>
 			<h1>Entrenamientos</h1>
 			<p className='text-sm text-gray-400 mt-2'>
 				Aquí se mostrarán todos tus entrenamientos registrados
 			</p>
-			<div className='flex flex-col gap-2 mt-8'>
-				<ResumeCard>
-					<ResumeCard.Header title='Leg Day' subTitle='June 7, 2024' />
-					<ResumeCard.Body>
-						<div className='flex flex-col gap-2 text-sm'>
-							<p className='text-gray-400'>Resumen de tu entrenamiento:</p>
-							<div className='flex items-center justify-between'>
-								<p className='flex gap-1 items-center'>
-									<DumbbellIcon size={14} />
-									<span>Curl de biceps</span>
-								</p>
-								<p className='flex gap-2'>
-									<span>3 series</span>
-								</p>
+			<WorkoutsSection workoutsToDisplay={createdWorkouts} />
+			{/* {createdWorkouts.map((workout) => (
+				<div key={workout.id}>
+					<h2>{workout.name}</h2>
+					<div>
+						{Object.keys(workout.sets).map((exerciseName) => (
+							<div key={exerciseName}>
+								<h3>{exerciseName}</h3>
+								<ul>
+									{workout.sets[exerciseName].map((set, index) => (
+										<li key={set.id}>
+											<span>Set {index + 1} - </span>
+											<span>
+												{set.weight} kg x {set.reps}
+											</span>
+										</li>
+									))}
+								</ul>
 							</div>
-							<div className='flex items-center justify-between'>
-								<p className='flex gap-1 items-center'>
-									<DumbbellIcon size={14} />
-									<span>Pull-ups</span>
-								</p>
-								<p className='flex gap-2'>
-									<span>3 series</span>
-								</p>
-							</div>
-							<div className='flex items-center justify-between'>
-								<p className='flex gap-1 items-center'>
-									<DumbbellIcon size={14} />
-									<span>Remo con barra</span>
-								</p>
-								<p className='flex gap-2'>
-									<span>3 series</span>
-								</p>
-							</div>
-						</div>
-						<Link href='#'>
-							<Button className='w-full mt-6' type='button'>
-								Ver entrenamiento
-							</Button>
-						</Link>
-					</ResumeCard.Body>
-				</ResumeCard>
-			</div>
+						))}
+					</div>
+				</div>
+			))} */}
 		</section>
 	);
 }
