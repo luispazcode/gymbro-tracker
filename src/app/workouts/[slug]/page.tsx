53ad2b7b5b950dcff1bbe5538a5f31a24c58a42a
Luis Paz Cordova <lpaz0073@gmail.com>
2024-08-05 22:09:48 -0500

    feat: add table shadcn component to workout detail page

--- src/app/workouts/[slug]/page.tsx
+++ src/app/workouts/[slug]/page.tsx
@@ -1,33 +1,71 @@
 import { getWorkoutBySlug } from "@/actions";
-import { Decrypt } from "@/utils";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { WorkoutDetail } from "@/interfaces";
+import { notFound, redirect } from "next/navigation";
 
 interface Props {
 	params: {
 		slug: string;
 	};
-	searchParams: {
-		id: string;
-	};
 }
 
-export default async function WorkoutDetailPage({
-	params,
-	searchParams,
-}: Props) {
-	const idEncrypted = Decrypt(searchParams.id);
-	const workoutDetail = await getWorkoutBySlug(params.slug, idEncrypted);
-	console.log(workoutDetail);
-	if (!workoutDetail) return <h1>Entrenamiento no encontrado...</h1>;
-	// return (
-	// 	<section>
-	// 		<h1>Entrenamiento: {workoutDetail.name}</h1>
-	// 		<p className='text-gray-400 mt-2'>
-	// 			Fecha:{" "}
-	// 			{workoutDetail.date?.toLocaleString("es-PE", { timeZone: "UTC" })}
-	// 		</p>
-	// 		<div>{Object.keys(workoutDetail.sets)}</div>
-	// 	</section>
-	// );
+export default async function WorkoutDetailPage({ params }: Props) {
+	const workoutDetail = (await getWorkoutBySlug(
+		decodeURIComponent(params.slug)
+	)) as WorkoutDetail;
+
+	if (!workoutDetail) redirect("/workouts");
+	return (
+		<section>
+			<h1>Entrenamiento: {workoutDetail.name}</h1>
+			<p className='text-gray-400 mt-2'>
+				Fecha: {workoutDetail.date.toLocaleString("es-PE", { timeZone: "UTC" })}
+			</p>
+			<div>
+				<div className='my-5'>
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead>Nombre del ejercicio</TableHead>
+								<TableHead className='text-center'>Serie</TableHead>
+								<TableHead className='text-center'>Repeticiones(N°)</TableHead>
+								<TableHead className='text-center'>Peso (KG)</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{Object.keys(workoutDetail.sets).map((exercise) => (
+								<TableRow key={exercise}>
+									<TableCell>{exercise}</TableCell>
+									<TableCell className='text-center space-y-4'>
+										{workoutDetail.sets[exercise].map((set, index) => (
+											<p key={set.id}>{index + 1}</p>
+										))}
+									</TableCell>
+									<TableCell className='text-center space-y-4'>
+										{workoutDetail.sets[exercise].map((set) => (
+											<p key={set.id}>{set.reps}</p>
+										))}
+									</TableCell>
+									<TableCell className='text-center space-y-4'>
+										{workoutDetail.sets[exercise].map((set) => (
+											<p key={set.id}>{set.weight}</p>
+										))}
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 // TODO: Show full workout detail
