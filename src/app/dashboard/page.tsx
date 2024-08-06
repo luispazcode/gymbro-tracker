8b39300eaf64ff668b3c79e99cde50fea61cb62b
Luis Paz Cordova <lpaz0073@gmail.com>
2024-08-05 22:44:01 -0500

    feat: Fetch recent workouts and exercises summary on DashboardPage load

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,59 +1,53 @@
+import { getExercisesSummary, getWorkouts } from "@/actions";
 import { ResumeCard } from "@/components";
 
-// [
-// 	{ workout: "Workout A", date: "2021-09-01" },
-// 	{ workout: "Workout B", date: "2021-09-02" },
-// 	{ workout: "Workout C", date: "2021-09-03" },
-// ];
-
-export default function DashboardPage() {
+export default async function DashboardPage() {
+	const recentWorkouts = await getWorkouts({ take: 3, orderByDate: "desc" });
+	const exercisesSummary = await getExercisesSummary({
+		take: 3,
+		orderByTotalSets: "desc",
+	});
 	return (
 		<div className='grid grid-cols-1 gap-6'>
 			<ResumeCard>
 				<ResumeCard.Header
-					title='Recent Workouts'
-					subTitle='Your recent workouts'
+					title='Últimos entrenamientos'
+					subTitle='Aquí figuran tus últimos entrenamientos registrados'
 				/>
 				<ResumeCard.Body>
 					<div className='flex flex-col gap-4'>
-						<div className='flex items-center justify-between text-sm'>
-							<p>Bicep Curls</p>
-							<p>11/05/2024</p>
-						</div>
-						<div className='flex items-center justify-between text-sm'>
-							<p>Squats</p>
-							<p>11/05/2024</p>
-						</div>
-						<div className='flex items-center justify-between text-sm'>
-							<p>Bench Press with Dumbbells</p>
-							<p>11/05/2024</p>
-						</div>
+						{recentWorkouts.map((workout) => (
+							<div
+								className='flex items-center justify-between text-sm'
+								key={workout.id}
+							>
+								<p>{workout.name}</p>
+								<p>{workout.date.toLocaleDateString()}</p>
+							</div>
+						))}
 					</div>
 				</ResumeCard.Body>
-				<ResumeCard.Link text='View all workouts' href='/workouts' />
+				<ResumeCard.Link text='Ir a tus entrenamientos' href='/workouts' />
 			</ResumeCard>
 			<ResumeCard>
 				<ResumeCard.Header
-					title='Completed Exercises'
-					subTitle='Exercises you have completed recently'
+					title='Resumen de tus ejercicios'
+					subTitle='Tus ejercicios con mas series completadas'
 				/>
 				<ResumeCard.Body>
 					<div className='flex flex-col gap-4'>
-						<div className='flex items-center justify-between text-sm'>
-							<p>Bicep Curls</p>
-							<p>12 reps</p>
-						</div>
-						<div className='flex items-center justify-between text-sm'>
-							<p>Squats</p>
-							<p>12 reps</p>
-						</div>
-						<div className='flex items-center justify-between text-sm'>
-							<p>Bench Press with Dumbbells</p>
-							<p>12 reps</p>
-						</div>
+						{exercisesSummary.map((exercise) => (
+							<div
+								className='flex items-center justify-between text-sm'
+								key={exercise.id}
+							>
+								<p>{exercise.name}</p>
+								<p>{exercise.sets.length} series</p>
+							</div>
+						))}
 					</div>
 				</ResumeCard.Body>
-				<ResumeCard.Link text='View all exercises' href='/exercises' />
+				<ResumeCard.Link text='Ir a tus ejercicios' href='/exercises' />
 			</ResumeCard>
 		</div>
 	);
