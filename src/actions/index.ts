8130c52944f05e82b036135daa610653dd2dea13
Luis Paz Cordova <lpaz0073@gmail.com>
2024-08-05 22:45:03 -0500

    feat: add getExercisesSummary action to barrel file

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,6 @@
 export { createExercise } from "./exercise/create-exercise";
 export { getExercises } from "./exercise/get-exercises";
+export { getExercisesSummary } from "./exercise/get-exercises-summary";
 
 export { getMuscleGroups } from "./muscle/get-muscle-groups";
 
