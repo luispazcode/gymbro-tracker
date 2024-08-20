3caf85bb8d81b94487d08ec604d8296913d572dd
Luis Paz Cordova <lpaz0073@gmail.com>
2024-08-20 18:22:20 -0500

    feat: add userId to createExercise action

--- src/actions/exercise/create-exercise.ts
+++ src/actions/exercise/create-exercise.ts
@@ -1,12 +1,18 @@
 "use server";
 
+import { auth } from "@/auth.config";
 import { CreateExerciseFormData } from "@/components/exercise/CreateExerciseForm";
 import prisma from "@/lib/prisma";
 
 export const createExercise = async (formData: CreateExerciseFormData) => {
 	try {
+		const session = await auth();
+		if (!session?.user) {
+			throw new Error("No hay una sesión activa");
+		}
 		const newExercise = await prisma.exercise.create({
 			data: {
+				userId: session.user.id,
 				name: formData.exerciseName,
 				tag: formData.exerciseName.toLowerCase().replace(/\s/g, "-"),
 				description: formData.description,
