1709107a8a5315fddbfd0e6f08599dd9f3fd46c6
Luis Paz Cordova <lpaz0073@gmail.com>
2024-08-20 18:23:14 -0500

    feat: filter exercises by user id

--- src/actions/exercise/get-exercises.ts
+++ src/actions/exercise/get-exercises.ts
@@ -1,10 +1,17 @@
 "use server";
 
+import { auth } from "@/auth.config";
 import prisma from "@/lib/prisma";
 
 export const getExercises = async () => {
 	try {
-		const createdExercises = await prisma.exercise.findMany();
+		const session = await auth();
+		if (!session) return [];
+		const createdExercises = await prisma.exercise.findMany({
+			where: {
+				userId: session.user.id,
+			},
+		});
 
 		return createdExercises;
 	} catch (error) {
