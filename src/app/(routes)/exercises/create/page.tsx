460c2f5541db8f3ecef0bb988f9c333fb123b572
Luis Paz Cordova <lpaz0073@gmail.com>
2024-08-22 16:37:02 -0500

    chore: purge old components unused

--- src/app/(routes)/exercises/create/page.tsx
+++ src/app/(routes)/exercises/create/page.tsx
@@ -14,45 +14,6 @@ export default async function CreateExercisePage() {
 				</div>
 				<CreateExerciseForm listMuscleGroups={muscleGroups} />
 			</div>
-
-			{/* <ResumeCard>
-				<ResumeCard.Header
-					title='Últimos ejercicios'
-					subTitle='Aquí puedes ver los últimos ejercicios que has creado'
-				/>
-				<ResumeCard.Body>
-					<div className='flex flex-col gap-2'>
-						<div className='flex items-center justify-between text-sm'>
-							<p className='flex gap-2 items-center'>
-								<BiDumbbell />
-								<span>Press banca con mancuernas</span>
-							</p>
-							<button className='bg-blue-600 p-2 rounded-sm hover:bg-blue-950'>
-								<Pencil color='white' size={16} />
-							</button>
-						</div>
-						<div className='flex items-center justify-between text-sm'>
-							<p className='flex gap-2 items-center'>
-								<BiDumbbell />
-								<span>Curl de biceps con barra Z</span>
-							</p>
-							<button className='bg-blue-600 p-2 rounded-sm hover:bg-blue-950'>
-								<Pencil color='white' size={16} />
-							</button>
-						</div>
-						<div className='flex items-center justify-between text-sm'>
-							<p className='flex gap-2 items-center'>
-								<BiDumbbell />
-								<span>Extensión de triceps con polea alta</span>
-							</p>
-							<button className='bg-blue-600 p-2 rounded-sm hover:bg-blue-950'>
-								<Pencil color='white' size={16} />
-							</button>
-						</div>
-					</div>
-				</ResumeCard.Body>
-				<ResumeCard.Link text='View all exercises' href='/exercises' />
-			</ResumeCard> */}
 		</section>
 	);
 }
