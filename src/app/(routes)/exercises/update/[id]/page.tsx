a304b16716b1d793946dff86ba28480760ca6f1e
Luis Paz Cordova <lpaz0073@gmail.com>
2024-08-24 11:57:28 -0500

    feat: add page for updating exercise details

---
+++ src/app/(routes)/exercises/update/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { getExerciseById, getMuscleGroups } from "@/actions";
+import { auth } from "@/auth.config";
+import { UpdateExerciseForm } from "@/components";
+
+interface Props {
+	params: {
+		id: string;
+	};
+}
+
+export default async function UpdateExercisePage({ params }: Props) {
+	const muscleGroups = await getMuscleGroups();
+	const session = await auth();
+	const exercise = await getExerciseById(params.id, session!.user.id);
+	if (!exercise) return <div>Exercise not found</div>;
+	return (
+		<section className='flex flex-col gap-6'>
+			<div className='flex flex-col gap-4 border border-gray-300 shadow-md p-6 rounded-md'>
+				<div>
+					<h2>Editar ejercicio</h2>
+					<p className='text-xs text-gray-400 mt-2'>
+						Vamos a actualizar la información del ejercicio:{" "}
+						<span className='font-semibold'>{exercise.name}</span>
+					</p>
+				</div>
+				<UpdateExerciseForm
+					listMuscleGroups={muscleGroups}
+					exerciseToEdit={exercise}
+				/>
+			</div>
+		</section>
+	);
+}
